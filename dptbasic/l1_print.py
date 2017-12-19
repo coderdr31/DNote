8fc8700f14aad34b5205c8e5e0b1a3aacb14250d
coderdr31 <1683751393@qq.com>
2017-12-19 22:42:25 +0800

    fresh

---
+++ dptbasic/l1_print.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+##################################################################
+## print()
+a = 1
+b = 'runoob'
+print(a,b)
+print("www","runoob","com",sep=".")  # 设置间隔符
+print("the num of %s is %d" % (b, a)) # 格式化输出
