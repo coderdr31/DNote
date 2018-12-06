23d43792befd6457a68f1d3f986d72fdfcdffdad
coderdr31 <1683751393@qq.com>
2018-12-06 22:03:16 +0800

    fresh

--- dptbasic/l2_set-sort-sorted-collections_Counter.py
+++ dptbasic/l2_set-sort-sorted-collections_Counter.py
@@ -14,3 +14,13 @@ xs.sort(key=len)  # short to long
 # len+reverse: long to short
 xs.sort(key=len,reverse=True)
 xs_new = sorted(xs,key=len,reverse=True)
+
+##################################################################
+## collections_Counter
+# Counter
+# Counter类的目的是用来跟踪值出现的次数。
+from collections import Counter
+a = [1, 2, 3, 1, 1, 2]
+result = Counter(a)
+print (result)
+# 还可以返回出现最多的n个,排序
